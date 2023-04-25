2e63a6784a1316aae3e2fda652c87c30c9aa5ea8
arturstaation <arturmackenzie12@gmail.com>
2023-04-25 17:03:02 +0000

    codigo comentado  e melhorado

--- ProjetoSerial/projetoserial.c
+++ ProjetoSerial/projetoserial.c
@@ -1,30 +1,23 @@
+//Tudo nesse código ta em double para evitar o erro que encontramos de dar nan (not a number) por tentar somar tipos diferentes de dados.
 #include <stdio.h>
 #include <stdlib.h>
-
 int main(void) {
+ long double n = 10000;
+ long double my_result = 0;
+ long double fatorial = 0; 
+// nao precisa ter uma lista, basta apenas uma variável já que so vamos acessar o ultimo termo ent so alterar a variável
+/*optamos por o uso da variável fatorial porque ela terá acesso rápido e não precisaremos recalcular
+toda vez valores que foram calculados anteriormente, já que o programa segue uma logica de ir
+aumentando em 1 os fatoriais , basta pegar o ultimo valor e multiplicar pelo atual */
+ for(int i =0; i<n; i++){
+ if(i >= 1) fatorial = fatorial*i;
 
-
-  long double n = 10000;
-  long double my_result = 0;
-  long double *fatorial;
-  fatorial = (long double*)calloc(n, sizeof(long double));
-
-  for(int i =0; i<n; i++){
-
-    
-    if(i >= 1) fatorial[i] = fatorial[i-1]*i;
-
-    
-    else {
-      fatorial[i] = 1;
-    }
-
-
-    
-    my_result  = my_result + (1/fatorial[i]);
-  }
-
-  printf("\n%.62Lf\n", my_result);
-  free(fatorial);
-  return 0;
+ else {
+ fatorial = 1;
+ }
+ my_result = my_result + (1/fatorial);
+ }
+// nao chamamos outras funções porque além de não precisar prejudica a velocidade.
+ printf("\n%.62Lf\n", my_result); // print com 62 casas pq era ate onde estava chegando, mas so esta certa ate a 18-19
+ return 0;
 }
--- ProjetoSerial/projetoserialcomgmp.c
+++ ProjetoSerial/projetoserialcomgmp.c
@@ -1,41 +1,32 @@
 #include <stdio.h>
-#include <gmp.h>
+#include <gmp.h> //biblioteca focada em aritmetica
 #include <stdlib.h>
-
 int main(void) {
-  mpf_set_default_prec(256);
+ mpf_set_default_prec(256); //precisao das contas , tudo a partir dessa linha vai ter essa precisao
+ mpf_t n; //declarer como tipo float da linguagem
+ mpf_init_set_ui(n, 10000); // atribuir valor a uma variavel
+ mpf_t my_result;
+ mpf_init_set_ui(my_result, 0);
+ mpf_t fatorial;
+ mpf_init_set_ui(fatorial, 0);
+ for(unsigned long int i = 0; i < mpf_get_ui(n); i++) {
+/*unsigned long int vai de 0 a 4.294.967.295, portanto por ser um expoente vai sentido ir de 0 a um
+numero alto para podermos testar a precisão e por o problema iniciar com o fatorial de um numero
+positivo. */
+ if(i >= 1)
+ mpf_mul_ui(fatorial, fatorial, i); // jeito de fazer a multiplicação da biblioteca (onde armazenar resultado, fator1, fator2);
+ else
+ mpf_set_ui(fatorial, 1);
+ mpf_t temp; // variavel temporária para armazenar o resultado de 1/n! (não sabia como colocar esse valor direto no add, no caso sem passar por um intermediario)
+ mpf_init(temp);
+ mpf_ui_div(temp, 1, fatorial); // jeito de fazer a divisão da biblioteca (onde armazenar resultado, numero, divisor)
+ mpf_add(my_result, my_result, temp);
+ mpf_clear(temp); // limpar espaço usado para variavel
+ }
+ gmp_printf("%.75Ff\n", my_result); // precisa alcançada com precisão
+ mpf_clear(my_result);
+ mpf_clear(n);
+ mpf_clear(fatorial);
 
-  mpf_t n;
-  mpf_init_set_ui(n, 10000);
-
-  mpf_t my_result;
-  mpf_init_set_ui(my_result, 0);
-
-  mpf_t *fatorial;
-  fatorial = (mpf_t*)calloc((unsigned long)mpf_get_ui(n), sizeof(mpf_t));
-  for(unsigned long i = 0; i < mpf_get_ui(n); i++) {
-    mpf_init(fatorial[i]);
-    if(i >= 1)
-      mpf_mul_ui(fatorial[i], fatorial[i-1], i);
-    else
-      mpf_set_ui(fatorial[i], 1);
-
-    mpf_t frac;
-    mpf_init(frac);
-    mpf_ui_div(frac, 1, fatorial[i]);
-    mpf_add(my_result, my_result, frac);
-    mpf_clear(frac);
-  }
-
-  gmp_printf("%.75Ff\n", my_result);
-
-  for(unsigned long i = 0; i < mpf_get_ui(n); i++) {
-    mpf_clear(fatorial[i]);
-  }
-  free(fatorial);
-
-  mpf_clear(my_result);
-  mpf_clear(n);
-  
-  return 0;
+ return 0;
 }
